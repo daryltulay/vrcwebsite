5672e6dcde019094fa79f4c9809575d9dd6b6bf5
daryl-webfaction <daryl.tulay@outlook.com>
2014-03-06 07:20:22 +0000

    Disabled responsiveness. Change container size from 1140px to 970px.

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -1,7 +1,7 @@
 <header class="banner" role="banner">
   <div class="container">
     <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+      <!-- <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"> -->
         <span class="sr-only">Toggle navigation</span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
@@ -10,7 +10,7 @@
      <!--  <a class="navbar-brand" href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a> -->
     </div>
 
-    <nav class="collapse navbar-collapse" role="navigation">
+    <nav role="navigation">
       <?php
         if (has_nav_menu('primary_navigation')) :
          wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav')); 
