2efdfaa1c8e249a6de66abdbdba8ab0d94d4181e
daryl-webfaction <daryl.tulay@outlook.com>
2014-03-07 11:45:31 +0000

    code rewrite

--- template-designservices.php
+++ template-designservices.php
@@ -3,4 +3,12 @@
 Template Name: Design Services Template
 */
 ?>
-
+<div class="container services-pad">
+<!-- Tab panes -->	
+	<div class="tab-content">
+		<div class="tab-pane active" id="about-us">
+		<?php get_template_part('templates/content-design-services');?>
+		<div class="services-grey-down"></div>	
+		</div>
+  </div>
+</div>
