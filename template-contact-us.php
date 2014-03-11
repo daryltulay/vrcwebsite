5014346fc9147ac985ea15b46e0cfafb60cb8e94
daryl-webfaction <daryl.tulay@outlook.com>
2014-03-11 08:20:26 +0000

    Updated Contact Page

--- template-contact-us.php
+++ template-contact-us.php
@@ -4,3 +4,11 @@ Template Name: Contact Us Template
 */
 ?>
 
+<div class="container services-pad">
+<!-- Tab panes -->	
+	<div class="tab-content">
+		<div class="tab-pane active" id="about-us">
+		<?php get_template_part('templates/content-contact');?>	
+		</div>
+  </div>
+</div>
