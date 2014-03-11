9dad3723e918441a73f50571f7895bc829a61faa
daryl-webfaction <daryl.tulay@outlook.com>
2014-03-11 08:21:00 +0000

    Updated CSS for contact page

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '40ee0b68a7e576c3d7590cd7dd29cfbe');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'b14ca5d93ccf3d91cd923076cf9884e7');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
---
+++ templates/content-contact.php
@@ -0,0 +1,32 @@
+<div class="row"  style="background-color: #fff;">
+				<div class="col-xs-3 column trianglebg">
+<div class="triangle-up-left"></div>
+<div class="covertriangle"></div>
+					
+				</div>
+				<div class="col-xs-5 column">
+                                         <h1 style="font-family: 'BebasNeueRegular';">ADDRESS</h1>
+					 <address> <strong>Twitter, Inc.</strong><br /> 795 Folsom Ave, Suite 600<br/> San Francisco, CA 94107<br /> <abbr title="Phone">P:</abbr> (123) 456-7890</address> 
+					 <div class="straightlineft"> </div>
+				</div>
+				<div class="col-xs-4 column">
+                                         <h1 style="font-family: 'BebasNeueRegular';">MOBILE</h1>					 <address> <strong>Twitter, Inc.</strong><br /> 795 Folsom Ave, Suite 600<br /> San Francisco, CA 94107<br /> <abbr title="Phone">P:</abbr> (123) 456-7890</address>
+                                          <h1 style="font-family: 'BebasNeueRegular';">EMAIL</h1>
+<p>connievroxas@yahoo.com</p>
+				</div>
+			</div>
+
+<div class="row"  style="background-color: #fff;">
+				<div class="col-xs-3 column" style="padding-left: 0px; background-image: url('http://lorempixel.com/220/340/'); height: 340px; background-repeat: no-repeat;">
+<div class="triangle-down-right"></div>
+				</div>
+				<div class="col-xs-5 column">
+                                         <h1 style="font-family: 'BebasNeueRegular';">MESSAGE US</h1>
+					 <address> <strong>Twitter, Inc.</strong><br /> 795 Folsom Ave, Suite 600<br /> San Francisco, CA 94107<br /> <abbr title="Phone">P:</abbr> (123) 456-7890</address>
+				</div>
+				<div class="col-xs-4 column">
+                                         <h3 style="font-family: 'BebasNeueRegular';">VRC PORTFOLIO DOWNLOAD</h3>					 <address> <strong>Twitter, Inc.</strong><br /> 795 Folsom Ave, Suite 600<br /> San Francisco, CA 94107<br /> <abbr title="Phone">P:</abbr> (123) 456-7890</address>
+                                          <h3 style="font-family: 'BebasNeueRegular';">LIKE US ON FACEBOOK</h3>
+				</div>
+			</div>
+<div style="height: 50px; background-color: #c3c3c4;">
