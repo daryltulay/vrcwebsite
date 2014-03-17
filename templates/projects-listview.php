39814f5e55f35abfa2597a731b1259f8314fcb33
daryl-webfaction <daryl.tulay@outlook.com>
2014-03-17 09:23:03 +0000

    Updated text.title 38-Storey Condotel Study, Added Mr and Mrs Alvin Ang in Three Storey House

--- templates/projects-listview.php
+++ templates/projects-listview.php
@@ -8,56 +8,61 @@
 					<h2>COMMERCIAL DESIGNS</h2>
 					&nbsp;
 					<ul class="list-unstyled" style="text-align: left; font-size: 12px;">
-							<li><a href="#">BOUTIQUE HOTEL <br>
+							<li><a href="#mg_ld_296" target="_blank">BOUTIQUE HOTEL <br>
 								Hi-Cool Philippines <br>
 								Libis, Quezon City</a></li>
 								&nbsp;
-							<li>STEEL ASIA <br>
+							<li><a href="#mg_ld_309" target="_blank">STEEL ASIA <br>
 							Canteen and Motorcycle Parking <br>
-							Calaca, Batangas</li>
+							Calaca, Batangas</a></li>
 							&nbsp;
 					
-							<li>38- STOREY CONDOTEL <br>
-							GOLDLAND PROPERTIES &amp; DEVELOPMENT CORP. <br>
-							Timog Avenue, Quezon City</li>
+							<li><a href="#mg_ld_310" target="_blank">38-Storey Condotel Study <br>
+							Timog Avenue, Quezon City</a></li>
 							&nbsp;
 					
-							<li>THREE- STOREY MIXED-USE COMMERCIAL BUILDING <br>
-							Antipolo City</li>
+							<li><a href="#mg_ld_283" target="_blank">THREE- STOREY MIXED-USE COMMERCIAL BUILDING <br>
+							Antipolo City</a></li>
 							&nbsp;
 					
-							<li>OFFICE TERMINAL BUILDING <br>
+							<li><a href="#mg_ld_289" target="_blank">OFFICE TERMINAL BUILDING <br>
 							Lipa City, Batangas <br>
-							STAR TOLLWAY INC.</li>
+							STAR TOLLWAY INC.</a></li>
 						</ul>
 					<h2>INSTITUTIONAL DESIGNS</h2>
 					&nbsp;
 					<ul class="list-unstyled" style="text-align: left; font-size: 12px;">  
-	<li>VILLEGAS-ROXAS CONSULTANTS BLDG. <br>
-Project 8, Quezon City</li>
+						<li><a href="#mg_ld_389" target="_blank">VILLEGAS-ROXAS CONSULTANTS BLDG. <br>
+							Project 8, Quezon City</a></li>
 
-&nbsp;
-	<li>AUF - College of Architecture Office <br>
-Angeles University Foundation</li>
-
-&nbsp;
-	<li>AUF - Learning Commons <br>
-University Library
-Angeles University Foundation</li>
-
-&nbsp;
-	<li>AMA BANK Financial Centre <br>
-311 Shaw Blvd., Mandaluyong City</li>
-
-&nbsp;
-	<li>AMA  Computer University - Rehabilitation <br>
-Main Campus, Project 8, Quezon City</li>
-
-&nbsp;
-
-	<li>ICP Parish Priest Office <br>
-Immaculate Conception Parish <br>
-Project 8, Quezon City</li></ul>
+							&nbsp;
+							<li><a href="#mg_ld_370" target="_blank">AUF - College of Architecture Office <br>
+								Angeles University Foundation</a></li>
+								
+								&nbsp;
+									<li><a href="#mg_ld_375" target="_blank">AUF - Learning Commons <br>
+								University Library
+								Angeles University Foundation</a></li>
+								
+								&nbsp;
+								<li><a href="#mg_ld_441" target="_blank">AUF – Professional School <br>
+								PS 307 and PS 308
+								Angeles University Foundation</a></li>
+								
+								&nbsp;
+									<li><a href="#mg_ld_347" target="_blank">AMA BANK Financial Centre <br>
+								311 Shaw Blvd., Mandaluyong City</a></li>
+								
+								&nbsp;
+									<li><a href="#mg_ld_359" target="_blank">AMA  Computer University - Rehabilitation <br>
+								Main Campus, Project 8, Quezon City</a></li>
+								
+								&nbsp;
+								
+									<li><a href="#mg_ld_383" target="_blank">ICP Parish Priest Office <br>
+								Immaculate Conception Parish <br>
+								Project 8, Quezon City</a></li>
+					</ul>
 				</article>
 			</div>
 			<div class="col-md-5 column">
@@ -65,46 +70,46 @@ Project 8, Quezon City</li></ul>
 					<h2>COMPETITIONS</h2>
 					&nbsp;
 					<ul class="list-unstyled" style="text-align: left; font-size: 12px;">
-						<li>SAGAY: Tubbataha Marine Life Sanctuary (International)<br>
+						<li><a href="#mg_ld_314" target="_blank">SAGAY: Tubbataha Marine Life Sanctuary (International)<br>
 						3<sup>rd</sup> Place Finalist<br>
-						Innovative Minds Architectural Design Competition: Virtual Context 2013</li>
+						Innovative Minds Architectural Design Competition: Virtual Context 2013</a></li>
 						
 						&nbsp;
-						<li>Build A Bench<br>
+						<li><a href="#mg_ld_323" target="_blank">Build A Bench<br>
 						International Design Competition 2013<br>
-						Kowloon East, Hong Kong</li>
+						Kowloon East, Hong Kong</a></li>
 						
 						&nbsp;
-						<li>Belgorod City Centre<br>
+						<li><a href="#mg_ld_331" target="_blank">Belgorod City Centre<br>
 						International Competition 2013<br>
-						Russia</li>
+						Russia</a></li>
 					</ul>
 					<h2>MASTER PLANNING</h2>
 					&nbsp;
 					<ul class="list-unstyled" style="text-align: left; font-size: 12px;">
-						<li>Minglanilla, Cebu - Reclamation<br>
-						100 hectares Coastal Reclamation</li>
+						<li><a href="#mg_ld_393" target="_blank">Minglanilla, Cebu - Reclamation<br>
+						100 hectares Coastal Reclamation</a></li>
 					</ul>
 					<h2>RESIDENTIAL DESIGNS</h2>
 					&nbsp;
 					<ul class="list-unstyled" style="text-align: left; font-size: 12px;">
-						<li>HERITAGE COUNTY HOMES <br>
+						<li><a href="#mg_ld_422" target="_blank">HERITAGE COUNTY HOMES <br>
 						5,000 sq.m. planned community <br>
 						With GoshenLand <br>
-						Baguio City, Philippines
+						Baguio City, Philippines</a>
 						</li>
 						
 						&nbsp;
-						<li>THREE-STOREY TOWNHOUSE <br>
-						Cebu City</li>
+						<li><a href="#mg_ld_428" target="_blank">THREE-STOREY TOWNHOUSE <br>
+						Cebu City</a></li>
 						
 						&nbsp;
-						<li>Seven -Storey Condotel <br>
-						Pagsanjan, Laguna</li>
+						<li><a href="#mg_ld_412" target="_blank">Seven -Storey Condotel <br>
+						Pagsanjan, Laguna</a></li>
 						
 						&nbsp;
-						<li>THREE - STOREY HOUSE <br>
-						Manila</li>
+						<li><a href="#mg_ld_410" target="_blank">THREE - STOREY HOUSE <br>
+						Mr. & Mrs. Alvin Ang <br> Manila</a></li>
 					</ul>
 				</article>
 			</div>
