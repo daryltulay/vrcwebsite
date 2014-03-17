13ec0d58011a6c20382f8340420dc708dd65f922
daryl-webfaction <daryl.tulay@outlook.com>
2014-03-17 09:23:33 +0000

    Updated CSS

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd42c8986966c66215ffd34f2cd10f2a6');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '76b725b37d4c0ebbaf3705c3e7f7bac7');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
