acb31b5b8b0e19f29a782e731ccf45149078acbd
daryl-webfaction <daryl.tulay@outlook.com>
2014-03-26 07:57:51 +0000

    Adjusted Footer Copyright text

--- templates/footer.php
+++ templates/footer.php
@@ -3,11 +3,11 @@
 		<div class="row">
 			<div class="col-xs-12">
 				<div class="row">
-					<div class="col-xs-4" style="padding-top: 9px;">
+					<div class="col-xs-5" style="padding-top: 9px;">
 						<?php dynamic_sidebar('sidebar-footer'); ?>
 						<p>&copy; VILLEGAS ROXAS CONSULTANTS ALL RIGHTS RESERVED /2013</p>	
 					</div> <!-- All rights reserved -->
-					<div class="col-xs-8">
+					<div class="col-xs-7">
 					<?php get_template_part('templates/footer-navbar'); ?></div> <!-- Footer nav -->
 				</div>
 			</div>
